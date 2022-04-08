f359735aba9a44b1d91151242de27513ac60823c
PaltosLP <motata.frochaux@gmail.com>
2022-04-08 12:40:31 +0200

    everything fixed

--- TicTacToe.py
+++ TicTacToe.py
@@ -18,14 +18,20 @@ def board_2(state):
     for i in range(len(state)):
         if state[i] == "x":
             state[i] = colored("x", "magenta")
-        elif state[i] == "o":
-            state[i] = colored("o", "green")
+        elif state[i] == "O":
+            state[i] = colored("O", "green")
     a1, a2, a3, a4, a5, a6, a7, a8, a9 = state[0], state[1], state[2], state[3], state[4], state[5], state[6], state[7], state[8]
     print(a7,"█",a8,"█",a9)
     print("▃▃█▃▃▃█▃▃")
     print(a4,"█",a5,"█",a6)
     print("▃▃█▃▃▃█▃▃")
     print(a1,"█",a2,"█",a3)
+
+def board(state, board_choice):
+    if board_choice == 1:
+        board_2(state)
+    else:
+        board_1(state)
 
 def win_cond(state, game_done):
     for i in range(len(state)):
@@ -63,12 +69,13 @@ def inp(player, state):
 
 
 def game():
+    board_choice = int(input('Thicc Board = 1\nThin Board = 2\n'))
     state = [1,2,3,4,5,6,7,8,9]
     game_done = win_cond(state, False)
     iswinner = False
     os.system('cls' if os.name == 'nt' else 'clear')
     for i in range(10):
-        board_1(state)
+        board(state,board_choice)
         win_cond(state, game_done)
         if gd:
             if i%2 == 0:
